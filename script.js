407b57248cb3fd28f8aa4a1f53f5d2f56571c55e
Arauco <arishel@gmail.com>
2019-12-10 13:45:34 -0800

    cleaned up code, added comments, wrote Readme

--- script.js
+++ script.js
@@ -1,28 +1,15 @@
 // Set the clock
 var clock = $('#clock');
-var day = $('#day');
-// var daysAry = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-var hour = $('#hour');
-// .fitText(1.3);
 function update() {
 	$('#clock').html(moment().format('MMMM DD YYYY H:mm:ss'));
-	$('#day').html(moment().day());
-	$('#hour').html(moment().format('H'));
-	//   moment().isoWeekday("Sunday");
 }
 setInterval(update, 1000);
 
-console.log(moment().format('HH'));
-
-
-
 // Create all necessary elements
 var hourArry = ["9:00 a.m.", "10:00 a.m.", "11:00 a.m.", "12:00 p.m.", "1:00 p.m.", "2:00 p.m.", "3:00 p.m.", "4:00 p.m.", "5:00 p.m.", ]
-var currentHour = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 var container = $(".container");
 
 var date = moment("12/25/1995", "MM-DD-YYYY");
-console.log(date.text);
 
 // FOR MULTI DAY PLANNER ADD NAVIGATE FUNCTIONS: PREV, TODAY, NEXT, AND SEARCH FIELD
 
@@ -30,11 +17,12 @@ console.log(date.text);
 // Build timeblocks
 // FOR MULTI DAY PLANNER ADD FUNCTION TO BE CALLED ON LOAD AND WHEN NAVIGATE BUTTONS ARE PRESSED
 for (let i = 0; i < hourArry.length; i++) {
-
+// New row for each block of time
 	var row = $("<div>");
 	row.addClass("row");
 	container.append(row);
 
+	// Time block with text of hour
 	var timeBlock = $("<div>");
 	timeBlock.addClass("time-block col-md-2");
 	timeBlock.text(hourArry[i]);
@@ -53,15 +41,16 @@ for (let i = 0; i < hourArry.length; i++) {
 	row.append(textArea);
 	
 
-	
-	if (currentHour[i] < parseInt(moment().format('HH'))) {
+	// Change style based on past, present, future
+	if (i+9 < parseInt(moment().format('HH'))) {
 		textArea.addClass("past");
-	} 	else if (currentHour[i] === parseInt(moment().format('HH'))) {
+	} 	else if (i+9 === parseInt(moment().format('HH'))) {
 		textArea.addClass("present");
-	} 	else if (currentHour[i] > parseInt(moment().format('HH'))) {
+	} 	else if (i+9 > parseInt(moment().format('HH'))) {
 		textArea.addClass("future");
 	}
 
+	// Save button
 	var saveBtn = $("<button>");
 	saveBtn.addClass("saveBtn col-md-1");
 	saveBtn.text("Save");
@@ -76,8 +65,3 @@ $(document).ready(function () {
 		localStorage.setItem($(this).parent().children("textarea").attr("id") , $(this).parent().children("textarea").val());
 	});
 });
-
-
-
-
-
